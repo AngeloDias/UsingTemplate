17e832ea069665cea6008106569b62cae6839b60
angelo.santana <angelossdias@gmail.com>
2020-10-29 14:13:11 -0300

    Adjusting html file and adding some php code

--- src/Controller/SiteController.php
+++ src/Controller/SiteController.php
@@ -3,13 +3,14 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 
 // References:
 // https://symfony.com/doc/current/components/asset.html
 // https://symfonycasts.com/screencast/symfony/assets
 class SiteController extends AbstractController
 {
-    public function indexAction()
+    public function indexAction(Request $request)
     {
         return $this->render('base.html.twig');
     }
